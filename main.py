2339718670f2ad90db7f60da2c93a5d1b01965a9
ibrown8 <43243853+ibrown8@users.noreply.github.com>
2019-10-05 22:30:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -147,33 +147,40 @@ def mult(instr):
     print('mult not done')
     
 def slt(instr):
-    print("{0} ${1}, ${2}, ${3} ".format(instr.name, instr.rd, instr.rs, instr.rt))
+    print("{0} ${1}, ${2}, ${3}".format(instr.name, instr.rd, instr.rs, instr.rt))
     #a = regfile.read(instr.rs)
     #b = regfile.read(instr.rt)
-    #if(a < b):
+    # if(a < b):
     #   regfile.write(instr.rd, 1)
     # else:
     #   regfile.write(instr.rd, 0)
     
 def xor(instr):
-    print("{0} ${1}, ${2}, ${3} ".format(instr.name, instr.rd, instr.rs, instr.rt))
-    #a = regfile.read(instr.rs)
+    print("{0} ${1}, ${2}, ${3}".format(instr.name, instr.rd, instr.rs, instr.rt))
+    #a = regfile.read(instr.rs) 
     #b = regfile.read(instr.rt)
     #regfile.write(instr.rd, a ^ b)
 def multu(instr):
-
+    print("{0} ${1}, ${2}".format(instr.name, instr.rs, instr.rt))
+    #a = regfile.read(instr.rs)
+    #b = regfile.read(instr.rt)
+    #c, d = divmod((a * b), (2^^32))
+    #regfile.writeHi(c)
+    #regfile.writeLo(d)
 def mfhi(instr):
-
+    print("{0} ${1}".format(instr.name, instr.rd))
+    #regfile.movefromHi(instr.rd)
 def mflo (instr):
-    
+    print("{0} ${1}".format(instr.name, instr.rd))
+    #regfile.movefromLo(instr.rd)
 def sll(instr):
-
+    print("{0} ${1}, ${2}, {3}".format(instr.name, instr.rd, instr.rt, instr.h))
 def srl(instr):
-
+    print("{0} ${1}, ${2}, {3}".format(instr.name, instr.rd, instr.rt, instr.h))
 def sltu(instr):
-
+    print("{0} ${1}, ${2}, ${3}".format(instr.name, instr.rd, instr.rs, instr.rt))
 def AND(instr):
-
+    print("{0} ${1}, ${2}, ${3}".format(instr.name, instr.rd, instr.rs, instr.rt))
 # i - types
 def addi(instr):
     print(instr.binary_S + '\n')
@@ -190,11 +197,11 @@ def ori(instr):
 
     
 def xori(instr):
-    print("{0} ${1}, ${2}, {3}\n".format(instr.name, instr.rt, instr.rs, instr,imm))
+    print("{0} ${1}, ${2}, {3}\n".format(instr.name, instr.rt, instr.rs, instr.imm))
     #a = regfile.read(instr.rs)
     #regfile.write(instr.rt, a ^ instr.imm)
 def lui(instr):
-    print(" {0} ${1},{2}").format(instr.name,instr.rt,instr.imm)
+    print("{0} ${1}, {2}").format(instr.name,instr.rt,instr.imm)
 
 
 
