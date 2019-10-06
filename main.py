2352d75da2bcaa3e1f33bf76b9adbb9d59073cd8
alooshie <43497838+alooshie@users.noreply.github.com>
2019-10-05 21:53:44 -0500

    added functions to dictionary

--- main.py
+++ main.py
@@ -33,10 +33,10 @@ class mem():
     def __init__(self, address, b0, b1, b2, b3): # this might be backwards... idk
 
         self.addr = address
-        self.b0 = b0# addr + 1
-        self.b1 = b1 #addr + 2
-        self.b2 = b2 #addr + 3
-        self.b3 = b3#addr + 4
+        self.b0 = b0 # addr + 1
+        self.b1 = b1 # addr + 2
+        self.b2 = b2 # addr + 3
+        self.b3 = b3 # addr + 4
         self.data = str(self.b3) + str(self.b2) + str(self.b1) + str(self.b0)
 
     #def write(self, addr):
@@ -48,7 +48,7 @@ class mem():
 # note, doesnt not work with negatives
     def writeMem(self,addres, value):
 
-        tmp = str( format(int(value),'08b'))
+        tmp = str( format(int(value),'016b'))
         # 0|0|0|0|0|0|0|0
         # 0,1,2,3,4,5,6,7
         #-8,7,6,5,4,3,2,1
@@ -63,6 +63,19 @@ class mem():
     #def loadMem(self,addr):
         #return self.data
 
+def find_memory_address(address):
+
+
+    if address[0:7] == '0x3000':
+
+        return 8192 + 4()
+
+    elif address[0:2] == '0x2':
+        to_int = (int(address[2:],16))
+        return to_int/20
+
+    else:
+        print('not supported')
 
 
 
@@ -85,9 +98,11 @@ class Instruction():
         if self.opcode == '000000':  # all r_types have this opcode, and function is the last 6 bits
             self.func = self.binary_S[26:32]
             self.type = 'r_type'
+            self.name = r_type[self.func][1]
         else:  # in this case the only else is type i
             self.func = self.opcode
             self.type = 'i_type'
+            self.name = i_type[self.func][1]
 
         # assign what the registers should be
         self.rs = int(self.binary_S[6:11], 2)
@@ -98,10 +113,6 @@ class Instruction():
             self.imm = -((int(self.binary_S[16:32], 2) ^ 0xFFFF) + 1)
         else:
             self.imm = int(self.binary_S[16:32], 2)
-        try:
-            self.name = func_dict[self.func][1]  # this will lookup the string name of the function in func_dict
-        except:
-            self.name = 'null'
 
     def printBinary(self):
         print(self.binary_S)
@@ -115,16 +126,16 @@ class Instruction():
 # r- types
 def add (instr):
     # addi rd,rs,rt
-    print(instr.binary_S + '\n' )
-    print( instr.name + " $" + str(instr.rd) +", $" + str(instr.rs) + ", $" + str(instr.rt) + '\n')
+    print(instr.binary_S  )
+    print( instr.name + " $" + str(instr.rd) +", $" + str(instr.rs) + ", $" + str(instr.rt) )
     #a = regfile.read(instr.rs)
     #b = regfile.read(instr.rt)
     #regfile.write(instr.rd, a + b)
 
 def OR(instr):
     # or rd, rs, rt
-    print(instr.binary_S + '\n')
-    print(instr.name + " $" + str(instr.rd) + ", $" + str(instr.rs) + ", $" + str(instr.rt) + '\n')
+    print(instr.binary_S )
+    print(instr.name + " $" + str(instr.rd) + ", $" + str(instr.rs) + ", $" + str(instr.rt) )
     #a = regfile.read(instr.rs)
     #b = regfile.read(instr.rt)
     #regfile.write(instr.rd, a | b)
@@ -132,11 +143,11 @@ def OR(instr):
 
 def mult(instr):
     # mult rs, rt
-    print(instr.binary_S + '\n')
+    print(instr.binary_S )
     print('mult not done')
     
-#def slt(instr):
-    #print("{0} ${1}, ${2}, ${3}\n".format(instr.name, instr.rd, instr.rs, instr.rt))
+def slt(instr):
+    print("{0} ${1}, ${2}, ${3} ".format(instr.name, instr.rd, instr.rs, instr.rt))
     #a = regfile.read(instr.rs)
     #b = regfile.read(instr.rt)
     #if(a < b):
@@ -144,8 +155,8 @@ def mult(instr):
     # else:
     #   regfile.write(instr.rd, 0)
     
-#def xor(instr):
-    #print("{0} ${1}, ${2}, ${3}\n".format(instr.name, instr.rd, instr.rs, instr.rt))
+def xor(instr):
+    print("{0} ${1}, ${2}, ${3} ".format(instr.name, instr.rd, instr.rs, instr.rt))
     #a = regfile.read(instr.rs)
     #b = regfile.read(instr.rt)
     #regfile.write(instr.rd, a ^ b)
@@ -153,34 +164,58 @@ def mult(instr):
 # i - types
 def addi(instr):
     print(instr.binary_S + '\n')
-    print(instr.name + " $" + str(instr.rt) + ", $" + str(instr.rs) + ", " + str(instr.imm) + '\n')
+    print(instr.name + " $" + str(instr.rt) + ", $" + str(instr.rs) + ", " + str(instr.imm) )
      #a = regfile.read(instr.rs) 
      #regfile.write(instr.rt, a + instr.imm)
 
 def ori(instr):
     print(instr.binary_S + '\n')
-    print(instr.name + " $" + str(instr.rt) + ", $" + str(instr.rs) + ", " + str(instr.imm) + '\n')
+    print(instr.name + " $" + str(instr.rt) + ", $" + str(instr.rs) + ", " + str(instr.imm) )
     # return instr.rt  or str.rs
     #a = regfile.read(instr.rs) 
     #regfile.write(instr.rt, a | instr.imm)
     
-#def xori(instr):
-    #print("{0} ${1}, ${2}, {3}\n".format(instr.name, instr.rt, instr.rs, instr,imm))
+def xori(instr):
+    print("{0} ${1}, ${2}, {3}\n".format(instr.name, instr.rt, instr.rs, instr,imm))
     #a = regfile.read(instr.rs)
     #regfile.write(instr.rt, a ^ instr.imm)
+def lui(instr):
+    print(" {0} ${1},{2}").format(instr.name,instr.rt,instr.imm)
+
+
 
 # python directory, like array, but uses "key" to instead of indices.
 # first couple lines ... add more
 # funciton table in a way..
+
+
 #               key,    [0],  [1]
-func_dict = {
+r_type = {
     # r - types:
     '100000': (add, 'add'),
     '100101': (OR, 'or'),
     '011000': (mult, 'mult'),
+    '011001': (multu,'multu'),
+    '010000': (mfhi,'mfhi'),
+    '010010':(mflo,'mflo'),
+    '100110':(xor,'xor'),
+    '000000': (sll,'sll'),
+    '000010': (srl,'srl'),
+    '101011':(sltu,'sltu'),
+    '100100':(AND,'and')
+}
+i_type = {
     # i-types:
     '001000': (addi, 'addi'),
-    '001101': (ori, 'ori')}
+    '001101': (ori, 'ori'),
+    '001110': (xori,'xori'),
+    '001111':(lui,'lui'),
+    '101000': (sb,'sb'),
+    '101011': (sw,'sw'),
+    '100000': (lb,'lb'),
+    '000100': (beq,'beq'),
+    '000101': (bne,'bne')
+}
 
 # define registers as dictionary
 hex_nums = { '0x0': 0,
@@ -273,8 +308,12 @@ def main():
 
         try:
             x = Instruction(to_hex)  # instance of the class with hex number
-            #print(x.name)
-            instructionFunc = func_dict[x.func][0]
+
+            if x.type == 'r_type':
+                instructionFunc = r_type[x.func][0]
+            else:
+                instructionFunc = i_type[x.func][0]
+
             instructionFunc(x)
         except:
             print("not supported")
@@ -289,24 +328,27 @@ def main():
     MemStart = 8192 #'0x2000'
     mem_Value = [] #mem(MemStart,0,0,0,0)
 
-    for i in range( 100): #this could work for each instruction instr_list: -> loop 1025
-
+    for i in range( 1025): #this could work for each instruction instr_list: -> loop 1025
         addr = str(hex(MemStart))
         mem_Value.append(mem(addr,0,0,0,0) )
         MemStart += 4   # increment addr by 4, each will have access to every bit.
 
 
-    l = 0
-    print('Address | (+0)  | (+4)  | (+8) | (+c)  | (+10)  | (+14) | (+18)  | (+1c)')
-    for row in mem_Value:
+#take as string..
+    # look at last 3 bits / 4 know index number
+    #
+    # l = 0
+    # print('Address | (+0)  | (+4)  | (+8) | (+c)  | (+10)  | (+14) | (+18)  | (+1c)')
+    # for row in mem_Value:
+    #
+    #     if int(row.addr,16) % (4*8) == 0 :
+    #          print( '\n', end= "")
+    #          print(str(row.addr)+ '|',end = " ")
+    #     # new way to write to memory. kinda slow because array O(N)
+    #     row.writeMem(row.addr, l +1)
+    #     l+= 1
+    #     row.printMem()
 
-        if int(row.addr,16) % (4*8) == 0 :
-             print( '\n', end= "")
-             print(str(row.addr)+ '|',end = " ")
-        # new way to write to memory. kinda slow because array O(N)
-        row.writeMem(row.addr, l + 1)
-        l+= 1
-        row.printMem()
 
 
     # the old way to change memory.
